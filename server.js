5fb4b818b1f15b45730eb25396639a13a77b816f
mohamedtarekrunprof <mohamedtarek.runprof@gmail.com>
2024-05-07 01:09:03 +0300

    auth id

--- server.js
+++ server.js
@@ -109,7 +109,8 @@ const signup = async (req, res) => {
 const findUser = async (req, res) => {
   try {
     const id = req.user.id;
-    const user = await admin.firestore().collection("users").doc(id).get();
+    const user = (await admin.firestore().collection("users").where(authId, "==", id).get())
+      .docs[0];
     if (!user) {
       return res.status(400).json({ message: "User not found", status: "failed", data: {} });
     }
